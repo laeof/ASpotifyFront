2cc29d92fd4b82831202d5e5fabb34e43917ca3e
laeof <turututuuuuuuu@gmail.com>
2024-09-10 11:16:38 +0300

    create component sketch

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { PlaylistType } from "../../dtos/playlist";
 
 @Component({
-  selector: 'app-create',
-  standalone: true,
-  imports: [],
-  templateUrl: './create.component.html',
-  styleUrl: './create.component.scss'
+    selector: 'app-create',
+    standalone: true,
+    imports: [],
+    templateUrl: './create.component.html',
+    styleUrl: './create.component.scss'
 })
 export class CreateComponent {
-
+    
 }
